b11130e208773595ab60558dceb586cd6cec9a0a
Soha Yusuf <102180459+SohaYusuf@users.noreply.github.com>
2026-03-15 14:26:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
+# python main.py --run_sor_as_solver 1 --num_episodes 3 
+# when N = 144 for training and testing:
+# python main.py --run_fgmres_as_solver 1 --num_episodes 100 --target_tol 1e-10 --n_actions 100 --learning_rate 0.001
+# python main.py --run_fgmres_as_solver 1 --mode test --train_RL_model 0
+
 import argparse
 import pdb
 import time
 import pprint
 
+from functions.solve_AD import solve_advection_diffusion
 from functions.utils import device
 from functions.test import test_policy_fgmres, test_policy_sor
+import torch
 
 
 def argparser():
@@ -34,7 +41,7 @@ def argparser():
                         help="Update rate of the target network")
     parser.add_argument("--learning_rate", type=float, default=1e-3, 
                         help="Learning rate for the AdamW optimizer")
-    parser.add_argument("--num_episodes", type=int, default=200, 
+    parser.add_argument("--num_episodes", type=int, default=3, 
                         help="Total number of episodes for training")
     parser.add_argument("--target_tol", type=float, default=1e-10, 
                         help="target tolerance for flexible GMRES")
@@ -72,17 +79,24 @@ def argparser():
     parser.add_argument("--n_actions", type=int, default=50, 
                         help="number of discrete actions the RL agent can take")  
 
-    parser.add_argument("--train_RL_model", type=int, default=True, 
+    parser.add_argument("--train_RL_model", type=int, default=False, 
                         help="Train RL agent with FGMRES")
-    parser.add_argument("--run_FGMRES_baseline", type=int, default=True, 
+    parser.add_argument("--run_FGMRES_baseline", type=int, default=False, 
                         help="Run FGMRES with no preconditioner")
-    parser.add_argument("--run_FGMRES_default_SOR", type=int, default=True, 
+    parser.add_argument("--run_FGMRES_default_SOR", type=int, default=False, 
                         help="Run FGMRES with default SOR")
     
     parser.add_argument("--run_sor_as_solver", type=int, default=False, 
                         help="Run SOR as solver (not preconditioner)")
-    parser.add_argument("--run_fgmres_as_solver", type=int, default=True, 
+    parser.add_argument("--run_fgmres_as_solver", type=int, default=False, 
                         help="Run FGMRES with SOR as preconditioner.")
+    
+    #------------------- hyperparameters for advection diffusion problem -------------#
+    parser.add_argument("--pure_diffusion_test", type=int, default=False, 
+                        help="Solve pure diffusion with c=0.")
+    parser.add_argument("--pure_advection_test", type=int, default=False, 
+                        help="Solve pure diffusion with mu=0.")
+    #---------------------------------------------------------------------------------#
 
     return parser.parse_args()
 
@@ -98,17 +112,21 @@ if __name__ == "__main__":
     pprint.pprint(vars(args))
     print()
 
+    solve_advection_diffusion(vars(args))
+    
     if args.run_fgmres_as_solver==True:
 
         from functions.train_fgmres import train_fgmres
+        model_path = "policy_net_weights.pth"
         if args.mode == "train":
-            
             # train
             start_time = time.time()
             policy_net, all_residuals_dict = train_fgmres(vars(args))
             elapsed_time = time.time() - start_time
             print(f"Total training time: {elapsed_time:.2f} seconds")
-    
+            # ---- SAVE MODEL ----
+            torch.save(policy_net.state_dict(), model_path)
+            print(f"Model saved to {model_path}")
             # test
             start_time = time.time()
             test_policy_fgmres(policy_net,
@@ -121,14 +139,14 @@ if __name__ == "__main__":
          
         from functions.train_sor import train_sor
         if args.mode == "train":
-            
+
             # this code works as of 10/27/2025 12:14 PM
             # train the SOR with fixed omega (check with theoretical omega for SPD system)
             start_time = time.time()
             policy_net, train_loader, all_residuals_dict = train_sor(vars(args))
             elapsed_time = time.time() - start_time
             print(f"Total training time: {elapsed_time:.2f} seconds")
-            
+
             # test
             start_time = time.time()
             test_policy_sor(policy_net,
@@ -147,8 +165,11 @@ if __name__ == "__main__":
             print(f"Total training time: {elapsed_time:.2f} seconds")
 
     elif args.mode == "test":
-        # evaluate
+        # test
+        policy_net, _ = train_fgmres(vars(args))
         start_time = time.time()
-        eval_(vars(args))
+        test_policy_fgmres(policy_net,
+            vars(args))
+        
         elapsed_time = time.time() - start_time
-        print(f"Total evaluation time: {elapsed_time:.2f} seconds")
+        print(f"Total testing time: {elapsed_time:.2f} seconds")
